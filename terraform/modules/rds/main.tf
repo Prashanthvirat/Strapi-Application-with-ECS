98e3a80ba2a63232642ef2288e0504ba823a6aab
Vankar Prashanth <prashanthvirat425@gmail.com>
2026-02-19 16:42:37 +0530

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -4,19 +4,20 @@ resource "aws_db_subnet_group" "prashanth_task7_db_subnet_group" {
 }
 
 resource "aws_db_instance" "prashanth_task7_db" {
-  identifier              = "prashanth-task7-strapi-db"
-  engine                  = "postgres"
-  engine_version          = "15"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
+  identifier        = "prashanth-task7-strapi-db"
+  engine            = "postgres"
+  engine_version    = "15"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
 
-  db_name                 = var.db_name
-  username                = var.db_username
-  password                = var.db_password
+  db_name     = var.db_name
+  username    = var.db_username
+  password    = var.db_password
 
-  publicly_accessible     = true
-  skip_final_snapshot     = true
-  deletion_protection     = false
+  publicly_accessible = true
+  skip_final_snapshot = true
+  deletion_protection = false
 
-  db_subnet_group_name    = aws_db_subnet_group.prashanth_task7_db_subnet_group.name
+  db_subnet_group_name = aws_db_subnet_group.prashanth_task7_db_subnet_group.name
 }
+
