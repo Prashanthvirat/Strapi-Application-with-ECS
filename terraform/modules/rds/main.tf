63a86909b1a8d7079bb860a72f85630060b4be8f
Vankar Prashanth <prashanthvirat425@gmail.com>
2026-02-19 17:09:01 +0530

    Change RDS instance class from db.t3.micro to db.t3.medium

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "prashanth_task7_db" {
   identifier        = "prashanth-task7-strapi-db"
   engine            = "postgres"
   engine_version    = "15"
-  instance_class    = "db.t3.micro"
+  instance_class    = "db.t3.medium"
   allocated_storage = 20
 
   db_name     = var.db_name
