be96e1deeabedad8529c88fa72837dbc5e659df3
prashanth <prashanthvirat425@gmail.com>
2026-02-19 15:41:40 +0530

    first commit

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,22 @@
+resource "aws_db_subnet_group" "prashanth_task7_db_subnet_group" {
+  name       = "prashanth-task7-db-subnet-group"
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_db_instance" "prashanth_task7_db" {
+  identifier              = "prashanth-task7-strapi-db"
+  engine                  = "postgres"
+  engine_version          = "15"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+
+  publicly_accessible     = true
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  db_subnet_group_name    = aws_db_subnet_group.prashanth_task7_db_subnet_group.name
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,12 @@
+image_tag         = "ee8b130"
+dockerhub_repo    = "prashanthtiffinbox/strapi-task7"
+execution_role_arn = "arn:aws:iam::778265708016:role/ecs_fargate_taskRole"
+
+subnet_ids = [
+  "subnet-04a2cc0ad31b80f28",
+  "subnet-01f2ca4d26de1fd81"
+]
+
+db_name     = "prashanth_task7_strapi"
+db_username = "prashanth_task7_user"
+db_password = "Prashanth#Task7$2026"
